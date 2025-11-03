38541d988ba189118cb65225c33e54c86a128d70
Richard Chavez <rchavezza@ucvvirtual.edu.pe>
2025-11-02 23:17:38 -0500

    chore(client): vincula Vite a host 0.0.0.0 (host: true) para evitar problemas IPv4/IPv6 en Windows

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true, // escucha en 0.0.0.0 (y localhost) para evitar issues IPv4/IPv6 en Windows
     port: 3000,
     strictPort: true,
     open: true,
